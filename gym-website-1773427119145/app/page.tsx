309bac033e5093a3861d0fac71060d41d5a7677d
ayanshaikh2491-stack <ayanshaikh2491@gmail.com>
2026-03-14 00:08:42 +0530

    Initial commit - NEXUS AI Generated

---
+++ gym-website-1773427119145/app/page.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+import Layout from '../app/layout';
+
+const Home: NextPage = () => {
+  return (
+    <Layout>
+      <h1>Welcome to Gym Website</h1>
+    </Layout>
+  );
+};
+
+export default Home;
